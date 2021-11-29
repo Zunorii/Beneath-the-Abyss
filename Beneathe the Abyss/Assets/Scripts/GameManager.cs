fa77918e95df48ae154d9f876095e247ad8c6f40
AlteredEidos <alteredeidos@gmail.com>
2021-11-29 13:42:45 -0800

    Respawn

--- Beneathe the Abyss/Assets/Scripts/GameManager.cs
+++ Beneathe the Abyss/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -14,5 +15,11 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public IEnumerator Restart()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
