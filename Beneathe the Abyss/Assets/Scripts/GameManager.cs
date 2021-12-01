46789dac9b64db10a3204af0bb967783838f57a4
AlteredEidos <alteredeidos@gmail.com>
2021-12-01 12:12:48 -0800

    game manager setup and player setup

--- Beneathe the Abyss/Assets/Scripts/GameManager.cs
+++ Beneathe the Abyss/Assets/Scripts/GameManager.cs
@@ -5,21 +5,52 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private bool paused;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (SceneManager.GetActiveScene().buildIndex != 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape) && paused == false)
+            {
+                paused = true;
+                Time.timeScale = 0;
+            }
+            else if (Input.GetKeyDown(KeyCode.Escape) && paused)
+            {
+                paused = false;
+                Time.timeScale = 1;
+            }
+        }
     }
-
     public IEnumerator Death()
     {
         yield return new WaitForSeconds(1);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void MainMenue()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
